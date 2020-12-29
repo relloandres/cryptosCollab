faaf879a0b0451a4a7bc1ea3da25ca589fd65f66
Andres Rello <arello@innomius.com>
2020-12-28 23:12:16 -0600

    chore: restart socket connection at the beginning of each day (code formatted)

--- dataAcquisition/BinanceAPI/webSockets/multiStreamListener3.py
+++ dataAcquisition/BinanceAPI/webSockets/multiStreamListener3.py
@@ -2,6 +2,7 @@ import csv
 import json
 from websockets import BinanceSocketManager
 from datetime import datetime
+import time
 
 
 STREAMS_PERIODS = {
@@ -19,14 +20,17 @@ STREAMS_PERIODS = {
     "_1d": 86400000,
     "_3d": 259200000,
     "_1w": 604800000
-  }
+}
 
 
 class streamListener:
 
     def __init__(self, streams_info):
+        # self.mocked_file_tag = 0
+
         # Instantiate a BinanceSocketManager
-        self.binanceClient = BinanceSocketManager()
+        self.binance_client = BinanceSocketManager()
+        self.current_connection = ''
 
         self.streams_info = streams_info
 
@@ -52,16 +56,30 @@ class streamListener:
             self.current_period_data[current_stream_name] = []
             self.current_file_tag[current_stream_name] = ''
 
-    def startListening(self):
-        conn = self.binanceClient.start_multiplex_socket(
+    def create_socket_connections(self):
+        self.current_connection = self.binance_client.start_multiplex_socket(
             self.streams_names, self.handle_msg)
 
-        # then start the socket manager
-        self.binanceClient.start()
+    def stop_socket_connection(self):
+        self.binance_client.stop_socket(self.current_connection)
 
-    def restartConnection(self):
-        self.binanceClient.stop_socket()
-    
+    def start_listening(self):
+        self.create_socket_connections()
+        # then start the socket manager
+        self.binance_client.start()
+
+    def restart_connection(self):
+        print("RESTARTING CONNECTION:")
+        self.stop_socket_connection()
+        print(" - Connections stopped")
+        time.sleep(3)
+        self.create_socket_connections()
+        print(" - Connections created")
+
+    def new_day_checkpoint(self, millis_time):
+        for stream_name in self.streams_names:
+            self.streams_last_checkpoint[stream_name] = millis_time
+
     def handle_msg(self, msg):
         current_stream_name = msg['stream']
         current_stream_data = msg['data']
@@ -69,35 +87,48 @@ class streamListener:
         if current_stream_name not in self.streams_names:
             print(f"Unknown stream name: {current_stream_name}")
 
-        current_date = datetime.fromtimestamp(current_stream_data['k']['t'] // 1000)
+        current_date = datetime.fromtimestamp(
+            current_stream_data['k']['t'] // 1000)
         new_day = (current_date.hour == 0) and (
             current_date.minute == 0) and (current_date.second == 0)
+        # new_day = current_date.minute % 2 == 0
 
         if self.first_file[current_stream_name]:
             self.first_file[current_stream_name] = False
             self.current_file_tag[current_stream_name] = current_stream_data['k']['t']
             self.streams_last_checkpoint[current_stream_name] = current_stream_data['k']['t']
-            print(f"Stream: {current_stream_name}, First file tag: {self.current_file_tag[current_stream_name]}, At: {current_date}")
+            print(
+                f"Stream: {current_stream_name}, First file tag: {self.current_file_tag[current_stream_name]}, At: {current_date}")
 
         if new_day:
             if (current_stream_data['k']['t'] != self.streams_last_checkpoint[current_stream_name]):
                 self._save_data(current_stream_name)
-                self.streams_last_checkpoint[current_stream_name] = current_stream_data['k']['t']
-                self.current_period_data[current_stream_name].append([current_stream_data['e'], current_stream_data['E'], current_stream_data['s'], current_stream_data['k']['i'], current_stream_data['k']['t'], current_stream_data['k']['T'], current_stream_data['k']['f'], current_stream_data['k']['L'], current_stream_data['k']['o'], current_stream_data['k']['c'], current_stream_data['k']['h'], current_stream_data['k']['l'], current_stream_data['k']['v'], current_stream_data['k']['q'], current_stream_data['k']['n'], current_stream_data['k']['x'], current_stream_data['k']['V'], current_stream_data['k']['Q'], current_stream_data['k']['B']])
+                self.new_day_checkpoint(current_stream_data['k']['t'])
+                self.current_period_data[current_stream_name].append([current_stream_data['e'], current_stream_data['E'], current_stream_data['s'], current_stream_data['k']['i'], current_stream_data['k']['t'], current_stream_data['k']['T'], current_stream_data['k']['f'], current_stream_data['k']['L'], current_stream_data['k']['o'],
+                                                                      current_stream_data['k']['c'], current_stream_data['k']['h'], current_stream_data['k']['l'], current_stream_data['k']['v'], current_stream_data['k']['q'], current_stream_data['k']['n'], current_stream_data['k']['x'], current_stream_data['k']['V'], current_stream_data['k']['Q'], current_stream_data['k']['B']])
                 self.current_file_tag[current_stream_name] = current_stream_data['k']['t']
-                print(f"Stream: {current_stream_name}, New file tag: {self.current_file_tag[current_stream_name]}, At: {current_date}")
-
+                print(
+                    f"Stream: {current_stream_name}, New file tag: {self.current_file_tag[current_stream_name]}, At: {current_date}")
+                self.restart_connection()
+                # self.mocked_file_tag += 1
         else:
-            trigger_save_process = (current_stream_data['k']['t']-self.streams_last_checkpoint[current_stream_name]) // self.streams_periods[current_stream_name] == self.streams_save_frecuency[current_stream_name]
+            trigger_save_process = (current_stream_data['k']['t']-self.streams_last_checkpoint[current_stream_name]
+                                    ) // self.streams_periods[current_stream_name] == self.streams_save_frecuency[current_stream_name]
             if trigger_save_process:
                 self._save_data(current_stream_name)
                 self.streams_last_checkpoint[current_stream_name] = current_stream_data['k']['t']
-                print(f"Stream: {current_stream_name}, File tag: {self.current_file_tag[current_stream_name]}, At: {current_date}")
-            
-            self.current_period_data[current_stream_name].append([current_stream_data['e'], current_stream_data['E'], current_stream_data['s'], current_stream_data['k']['i'], current_stream_data['k']['t'], current_stream_data['k']['T'], current_stream_data['k']['f'], current_stream_data['k']['L'], current_stream_data['k']['o'], current_stream_data['k']['c'], current_stream_data['k']['h'], current_stream_data['k']['l'], current_stream_data['k']['v'], current_stream_data['k']['q'], current_stream_data['k']['n'], current_stream_data['k']['x'], current_stream_data['k']['V'], current_stream_data['k']['Q'], current_stream_data['k']['B']])
+                print(
+                    f"Stream: {current_stream_name}, File tag: {self.current_file_tag[current_stream_name]}, At: {current_date}")
+
+            self.current_period_data[current_stream_name].append([current_stream_data['e'], current_stream_data['E'], current_stream_data['s'], current_stream_data['k']['i'], current_stream_data['k']['t'], current_stream_data['k']['T'], current_stream_data['k']['f'], current_stream_data['k']['L'], current_stream_data['k']['o'],
+                                                                  current_stream_data['k']['c'], current_stream_data['k']['h'], current_stream_data['k']['l'], current_stream_data['k']['v'], current_stream_data['k']['q'], current_stream_data['k']['n'], current_stream_data['k']['x'], current_stream_data['k']['V'], current_stream_data['k']['Q'], current_stream_data['k']['B']])
 
     def _save_data(self, stream_name):
-        file_name = self.streams_destination_path[stream_name] + '/' + stream_name + '-' + str(self.current_file_tag[stream_name]) + '.csv'
+        file_name = self.streams_destination_path[stream_name] + '/' + \
+            stream_name + '-' + \
+            str(self.current_file_tag[stream_name]) + '.csv'
+        # file_name = self.streams_destination_path[stream_name] + \
+        #     '/' + stream_name + '-' + str(self.mocked_file_tag) + '.csv'
 
         with open(file_name, mode='a', newline='') as klines_file:
             klines_writer = csv.writer(klines_file, delimiter=',')
@@ -108,17 +139,23 @@ class streamListener:
         self.current_period_data[stream_name] = []
 
 
-
 # Load streams info
-with open("/home/pi/andres/cryptos/cryptosCollab/dataAcquisition/BinanceAPI/webSockets/myStreams.json") as json_file:
+with open("/Users/Innomius/Andres/personal/myGithub/cryptosCollab/dataAcquisition/BinanceAPI/webSockets/myStreams.json") as json_file:
     my_streams_info = json.load(json_file)
 
 # Instantiate a client
 klines_bm = streamListener(my_streams_info)
 
-# Satrt listening 
-klines_bm.startListening()
+# Satrt listening
+klines_bm.start_listening()
 
+# print("Stopping connection")
+# klines_bm.binance_client.stop_socket(klines_bm.current_connection)
+# print(" - Connections ended")
+# klines_bm.binance_client.reactor.callFromThread(
+#     klines_bm.binance_client.reactor.stop)
+# klines_bm.binance_client.join()
+# print(" - Thread ended")
 
 # # stop the socket manager
 # klines_bm.stop_socket(conn_key)
